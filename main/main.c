0a7a0d9d46613b2e78d6b3921cc46dd8d40700da
豚豚 <81475239+irukatun@users.noreply.github.com>
2026-04-15 10:13:04 +0800

    style(main): 修正註解錯字「因此」

--- main/main.c
+++ main/main.c
@@ -9,14 +9,14 @@
 #include "module_boot.h"
 
 // 日誌標籤
-static const char *TAG = "Main";
+static const char *TAG = "Main"; 
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "=====系統啟動中=====");
 
     // GPIO3 為 ESP32-S3 JTAG 啟動配置引腳，控制 GPIO39–42 的 JTAG 功能是否啟用
-    // 避免 GPIO3 啟動時狀態無法確認導致 JTAG 狀態殘留，引此進行防禦性釋放
+    // 避免 GPIO3 啟動時狀態無法確認導致 JTAG 狀態殘留，因此進行防禦性釋放
     gpio_reset_pin(GPIO_NUM_39);
     gpio_reset_pin(GPIO_NUM_40);
     gpio_reset_pin(GPIO_NUM_41);
